8a6663b395fbf6311f11078b0e85886a9efa22c8
Markus Korn <markus.korn@gmail.com>
2024-03-25 13:45:14 +0100

    Initial Commit

---
+++ sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleApp
+//
+//  Created by Korn Markus on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
